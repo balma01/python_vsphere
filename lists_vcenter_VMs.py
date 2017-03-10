ac7f6543efa08c85dd2e7b8d3496d6a4ea2849e2
balma01 <mauro.balbiani@gmail.com>
2017-03-10 17:21:36 +0100

    Update lists_vcenter_VMs.py

--- lists_vcenter_VMs.py
+++ lists_vcenter_VMs.py
@@ -1,8 +1,11 @@
+# https://www.vcloudnine.de/first-steps-with-python-and-pyvmomi-vsphere-sdk-for-python/
+#
 # PREREQ: install pyvmomi (pip install pyvmomi)
 """
 Python program for listing the vms vCenter
 """
-
+# To connect to the vSphere API, we have to import and use the module pyVim,
+# more precise, the pyVim.connect module and the SmartConnect function. 
 from pyVim.connect import SmartConnect, Disconnect
 import ssl
 
