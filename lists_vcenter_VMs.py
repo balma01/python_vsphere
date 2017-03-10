67c5570499df09a83465ee0db74ed48765045145
balma01 <mauro.balbiani@gmail.com>
2017-03-10 17:02:24 +0100

    Update lists_vcenter_VMs.py

--- lists_vcenter_VMs.py
+++ lists_vcenter_VMs.py
@@ -5,15 +5,20 @@ Python program for listing the vms vCenter
 
 from pyVim.connect import SmartConnect, Disconnect
 import ssl
-vcenter = input ("insert vcenter FQDN: ")
-userid  = input ("insert userid: ")
-password= input ("insert password: ")
+
+vcenter = input ("type vcenter FQDN: ")
+userid  = input ("type userid: ")
+password = input ("type password: ")
+
 s = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 s.verify_mode = ssl.CERT_NONE
+
 c = SmartConnect(host=vcenter, user=userid, pwd=password, sslContext=s)
 datacenter = c.content.rootFolder.childEntity[0]
 vms = datacenter.vmFolder.childEntity
+
 for i in vms:
     print(i.name)
-pause = input ("Press to end ")
+pause = input ("Press ENTER to end ")
+
 Disconnect(c)
